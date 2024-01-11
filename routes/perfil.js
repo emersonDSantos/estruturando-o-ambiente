f0abe949bc2495dbf5924370fb1f6ec1e8c7eff8
Emerson Santos <e-ti@msn.com>
2024-01-11 02:22:47 -0300

    Commit3

--- routes/perfil.js
+++ routes/perfil.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Perfil } = require('../models');
 const router = express.Router();
 router.get('/', (_, res) => {
-    Perfil.find({}).then((perfil) => {
+    Perfil.find({}).limit(1).then((perfil) => {
         res.render('perfil/index', {
             perfil: perfil
         });
